c5d25c652367d462197504fde2d13509a11b207e
NikoBoolean <niko.kacic@gmail.com>
2025-01-27 11:40:20 -0300

    changes in the HTML for the beginning of the second practice exercise, Bootstrap was added to index.html

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list',
+  selector: 'app-hero-list',
   standalone: false,
-  
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })

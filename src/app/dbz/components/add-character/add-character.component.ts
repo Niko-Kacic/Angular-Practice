bddbd4f4b50dc00dbca6997eb4f4ceefda3d340a
NikoBoolean <niko.kacic@gmail.com>
2025-01-28 18:17:10 -0300

    small adjustment to the reallocation of initial values in a single line

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -11,20 +11,18 @@ export class AddCharacterComponent {
   @Output()
   public onNewCharacter : EventEmitter<Character> = new EventEmitter();
 
-
   public character: Character = {
     name: '',
     power: 0
   }
 
   emitCharacter():void{
-    console.log(this.character)
+
     if (this.character.name.length===0) return;
 
     this.onNewCharacter.emit(this.character);
 
-    this.character.name = '';
-    this.character.power = 0;
+    this.character = {name: '', power: 0}
   }
 
 }
